d131ea983742e025e288d12c40769f9138e48a0a
moriroKim <moriroKim@gmail.com>
2024-08-19 11:35:01 +0900

    ✅ 포켓몬 데이터 fetch코드 추가

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,32 @@ import client from './client';
 
 function App() {
     useEffect(() => {
-        const fetchAPI = async () => {
+        const numberArray = Array.from({ length: 151 }, (_, idx) => idx + 1);
+        const fetchPokemonAPI = async (pokemonId) => {
             try {
-                const response = await client.get('/pokemon-species/1');
-                console.log(response.data);
+                const pokemonTextResponse = await client.get(`/pokemon-species/${pokemonId}`);
+                const pokemonImgResponse = await client.get(`/pokemon-form/${pokemonId}`);
+                const textData = pokemonTextResponse.data;
+                const imgData = pokemonImgResponse.data.sprites;
+                const pokemonData = {
+                    id: pokemonId,
+                    name: textData.names.find((el) => el.language.name === 'ko').name,
+                    description: textData.flavor_text_entries.find((el) => el.language.name === 'ko').flavor_text,
+                    frontImg: imgData.front_default,
+                    backImg: imgData.back_default,
+                };
+                return pokemonData;
             } catch (error) {
                 console.error('Error fetching data:', error);
             }
         };
-        fetchAPI();
+
+        const fetchPokemonData = async () => {
+            const pokemonInfo = await Promise.all(numberArray.map((el) => fetchPokemonAPI(el)));
+            console.log(pokemonInfo);
+        };
+
+        fetchPokemonData();
     }, []);
 
     return <></>;
